16a68fe314deda8cb4af3f73a5c4fe7187d6ac40
jamesfrohnhofer <fijimf@gmail.com>
2026-04-03 14:33:07 -0400

    fix: scope QuoteModelAdvice to @Controller beans only to avoid DB queries on REST requests

--- src/main/java/com/yotto/basketball/controller/QuoteModelAdvice.java
+++ src/main/java/com/yotto/basketball/controller/QuoteModelAdvice.java
@@ -2,10 +2,11 @@ package com.yotto.basketball.controller;
 
 import com.yotto.basketball.entity.Quote;
 import com.yotto.basketball.service.QuoteService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-@ControllerAdvice
+@ControllerAdvice(annotations = Controller.class)
 public class QuoteModelAdvice {
 
     private final QuoteService quoteService;
